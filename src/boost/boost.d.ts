805671c9fd784a89ca115f2eb0593aa9c30b7fdc
frostime <frostime@foxmail.com>
2023-08-14 18:23:32 +0800

    init

---
+++ src/boost/boost.d.ts
@@ -0,0 +1,1 @@
+type ArgType = "wsmain-click-blockicon";
---
+++ src/boost/index.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 by Yp Z (frostime). All Rights Reserved.
+ * @Author       : Yp Z
+ * @Date         : 2023-08-14 18:07:29
+ * @FilePath     : /src/boost/index.ts
+ * @LastEditTime : 2023-08-14 18:09:26
+ * @Description  : boost
+ */
+
+
+
+export function argparser(type: ArgType, args: any) {
+    if (type === "wsmain-click-blockicon") {
+        let detail = args?.detail;
+        if (!detail) {
+            detail = args;
+        }
+    }
+}
